4f4cbac1fd64c535354a8df23c61d954ea41fb74
FribbulusXax <a.karamyshev@gmail.com>
2020-11-21 19:07:12 +0300

    add article title preview

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
-import webbrowser
+from bs4 import BeautifulSoup
+import requests
 
 df = pd.read_csv('wikilinks.csv', header=None)
 
 def get_link():
     return(df.sample().iloc[0][0])
 
+def get_title(link):
+    req = requests.get(link)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    return(soup.find('h1').get_text())
+
 st.title("Looking for an interesting or weird article from Wikipedia?")
 if st.button("Yep!"):
-    #webbrowser.open_new_tab(str(get_link()))
     url = str(get_link())
-    link = '[click me](' + url + ')'
+
+    link = '['+ get_title(url) +'](' + url + ')'
     st.markdown(link, unsafe_allow_html=True)
